1537b109331ec6b4d210f5122e452e18ed196301
JasonG <jason.garcia31@edu.cna.nl.ca>
2025-11-23 16:53:31 -0330

    Testing how to update python files via git/github to track project.

---
+++ git.py
@@ -0,0 +1,24 @@
+def title():
+    print("Black '21' Jack")
+    print()
+
+
+
+def main():
+    while True:
+
+
+
+
+
+
+
+        choice = input("Go again? (y/n): ")
+        if choice == "n":
+            print()
+            print("Bye")
+            break
+
+
+if __name__ == "__main__":
+    main()
