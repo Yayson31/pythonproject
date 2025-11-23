66d774a6311ce9ac8bd1a96d5868291bf2d32611
JasonG <jason.garcia31@edu.cna.nl.ca>
2025-11-23 18:10:21 -0330

    more pushing tests with git and began creating foundation of project, working on creating and shuffling a deck of cards.

--- git.py
+++ git.py
@@ -1,11 +1,38 @@
-def title():
-    print("Black '21' Jack")
-    print()
+import random
 
+def title():
+    print("BLACKJACK!")
+    print("Blackjack payout is 3:2")
+
+def create_deck(deck):
+    for suit in ["C", "D", "H", "S"]:
+        for rank in ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]:
+            deck.append(rank+suit)
+
+
+
+def shuffle_deck(deck):
+    random.shuffle(deck)
+
+
+'''def play_deck():'''
+
+
+    
 
 
 def main():
+    title()
+    bank = 100.0
     while True:
+        print(f"Money: {bank}")
+        bet = float(input(f"Bet: "))
+        
+        deck = []
+        create_deck(deck)
+        shuffle_deck(deck)
+        print(*deck, sep="\n")
+        
 
 
 
