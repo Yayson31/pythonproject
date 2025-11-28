f825ae084b6b8caf3101a2ff85eb5674ff3e2860
JasonG <jason.garcia31@edu.cna.nl.ca>
2025-11-28 18:15:54 -0330

    Cleaned up create deck function. Added two lists, one for each hand in play (player,house). Added separate function for inital card deal, which displays correctly. Need to figure out: 1. how to store value in each card to tally up score and to determine next move based on BlackJack rules. 2. code around betting and setting up a player bank.

--- git.py
+++ git.py
@@ -2,19 +2,30 @@ import random
 
 def title():
     print("BLACKJACK!")
+    
     print("Blackjack payout is 3:2")
     print()
 
 def create_deck(deck):
-    for suit in ["C", "D", "H", "S"]:
-        for rank in ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]:
-            deck.append(rank+suit)
+    suit = ["C", "D", "H", "S"]
+    rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+    for s in suit:
+        for r in rank:
+            deck.append(r+s)
 
 
 
 def shuffle_deck(deck):
     random.shuffle(deck)
 
+
+def card_deal(deck,deck2):
+    card = deck.pop()
+    deck2.append(card)
+    card = deck.pop()
+    deck2.append(card)
+    return deck2
 
 '''def play_game(deck):'''
     
@@ -23,22 +34,46 @@ def shuffle_deck(deck):
 
 def main():
     title()
-    bank = 100.0
+    
     while True:
-        print(f"Money: {bank}")
-        bet = float(input(f"Bet: "))
+        #print(f"Money: {bank}")
+        #bet = float(input(f"Bet: "))
         print()
         
         deck = []
+        player_hand = []
+        house_hand = []
+        
         create_deck(deck)
         shuffle_deck(deck)
 
-        player_card1 = random.choice(deck)
-        cpu_card1 = random.choice(deck)
-        player_card2 = random.choice(deck)
-        cpu_card2 = random.choice(deck)
+        player_hand = card_deal(deck, player_hand)
+        house_hand = card_deal(deck, house_hand)
+        
+        print(f"DEALER'S SHOW CARD:\n{house_hand[0]}")
+        print()
+        print(f"Your Cards:\n{player_hand}")
+        print()
+    
+        
+        print(len(deck))
+        print(deck)
 
-        print(f"DEALER'S SHOW CARD:\n{cpu_card1}")
+        '''player_card = deck.pop()
+        player_card = deck.pop()
+        house_card = deck.pop()
+        player_hand.append(play_card)
+        player_hand.append(play_card)
+        player_hand.append(play_card)'''
+        
+        
+
+        '''cpu_card1 = random.choice(deck)
+        player_card2 = random.choice(deck)
+        cpu_card2 = random.choice(deck)'''
+        
+
+        '''print(f"DEALER'S SHOW CARD:\n{cpu_card1}")
         print()
         print(f"Your Cards:\n{player_card1}\n{player_card2}")
         print()
@@ -50,7 +85,7 @@ def main():
                 print(player_card3)
                 continue
             else:
-                break
+                break'''
                 
         
 
